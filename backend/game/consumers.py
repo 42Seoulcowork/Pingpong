e8be291cd7023b855d9b537ea3f5998507472efc
hujeong <hujeong@student.42seoul.kr>
2024-04-17 14:07:00 +0900

    Feat: local 게임 인증

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,12 +1,41 @@
 import asyncio
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+import django
+django.setup()
+from api.models import User
 
 class GameConsumer(AsyncJsonWebsocketConsumer):
+    @database_sync_to_async
+    def getModel(self):
+        return User.objects.get(intra_id=self.user.intra_id)
+    
+    @database_sync_to_async
+    def setGameStatus(self, status):
+        self.model.game_status = status
+        self.model.save()
+
     async def connect(self):
-        await self.accept()
+        self.user = self.scope['user']
+        if self.user.is_authenticated:
+            self.model = await self.getModel()
+            if self.model.game_status == True:
+                await self.close()
+                return
+            await self.setGameStatus(True)
+            await self.accept()
+            print('connected ' + self.model.intra_id + ': ' + str(self.model.game_status))
+        else:
+            await self.close()
 
     async def disconnect(self, close_code):
+        await self.setGameStatus(False)
+        print("disconnected " + self.model.intra_id + ": " + str(self.model.game_status))
         self.connected = False
+        await self.close()
 
     async def receive_json(self, content, **kwargs):
         self.connected = True
